39280e4662eafd55aacac42499bf04c992fc3b78
Dimityr <dzambazov1986@gmail.com>
2024-02-26 19:20:46 +0200

    Add project files.

---
+++ Problem2/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+// Calculate the average value of the elements in the middle of the array
+int midIndex = numbers.Length / 2;
+double average = (numbers[midIndex - 1] + numbers[midIndex]) / 2.0;
+
+// Print the result formatted to the second digit
+Console.WriteLine($"{average:F2}");
